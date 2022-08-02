69912f305255f487ee712dde6a4bebfc6f14b29d
Raveendra Tudangil <ravee@Raveendras-MacBook-Pro.local>
2022-08-02 18:56:24 +0530

    added fragments for the layout :card_file_box:

---
+++ app/src/main/java/io/raveerocks/asteroidradar/ui/fragments/DetailFragment.kt
@@ -0,0 +1,5 @@
+package io.raveerocks.asteroidradar.ui.fragments
+
+import androidx.fragment.app.Fragment
+
+class DetailFragment : Fragment()
---
+++ app/src/main/java/io/raveerocks/asteroidradar/ui/fragments/MainFragment.kt
@@ -0,0 +1,7 @@
+package io.raveerocks.asteroidradar.ui.fragments
+
+import androidx.fragment.app.Fragment
+
+
+
+class MainFragment : Fragment()
